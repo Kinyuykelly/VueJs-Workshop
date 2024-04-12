37d6e88890203fdf067ea639b60967915cac1d25
kinyuykelly <kinyuykelly213@gmail.com>
2024-04-12 17:17:59 +0100

    counterapp

---
+++ CounterApp/src/App.vue
@@ -0,0 +1,37 @@
+
+<script setup> 
+import { ref } from "vue"
+
+let count = ref(0);
+ const addToCount = () => {
+  count.value = count.value + 1;
+ }
+ const subtractFromCount = () => {
+  count.value = count.value - 1;
+ }
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The count is ...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main{
+  background: aliceblue;
+  height: 100vh;
+  height: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+div{
+  text-align: center;
+}
+</style>
